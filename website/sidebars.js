4a10392ef69f141a4d45b9fddff4356462ad58fd
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2020-02-09 15:44:11 +0100

    add website source code

---
+++ website/sidebars.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  someSidebar: {
+    "Problem Domain": ['doc1', 'doc2', 'doc3'],
+    Methodologies: ['mdx'],
+    Interoperability: ['mdx'],
+    DPF: ['mdx'],
+    Contribution: ['mdx'],
+    "Publication": ['mdx'],
+    Prototype: ['mdx'],
+    "Future Direction": ['mdx', 'doc1'],
+  },
+};
