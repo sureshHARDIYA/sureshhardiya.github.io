bc5958251558d5c9d3ae476181eb08602eac256f
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2020-02-09 15:45:12 +0100

    Deploy website version based on 4a10392ef69f141a4d45b9fddff4356462ad58fd

--- website/.docusaurus/client-modules.js
+++
@@ -1,4 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("/Users/sureshkumarmukhiyahvl/Documents/Projects/sureshhardiya.github.io/website/src/css/custom.css"),
-];
